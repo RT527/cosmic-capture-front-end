fc6b7f7c05f4ef52e962038108da9a78417ee0d8
RT527 <rafi.talukder7@gmail.com>
2023-06-05 09:42:04 -0400

    image update

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,19 +1,49 @@
-// css
+import React, { useEffect, useState } from 'react'
 import styles from './Landing.module.css'
-
-// types
 import { User } from '../../types/models'
 
 interface LandingProps {
-  user: User | null;
+  user: User | null
 }
 
-const Landing = (props: LandingProps): JSX.Element => {
-  const { user } = props
+interface EpicData {
+  url: string
+  caption: string
+}
+
+const Landing: React.FC<LandingProps> = ({ user }) => {
+  const [epicData, setEpicData] = useState<EpicData | null>(null)
+
+  useEffect(() => {
+    fetchMostRecentEpicImage()
+  }, [])
+
+  const fetchMostRecentEpicImage = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/epic/image')
+      if (response.ok) {
+        const data = await response.json()
+        setEpicData({ url: data.url, caption: data.caption })
+      } else {
+        console.error('Error fetching EPIC image:', response.status, response.statusText)
+      }
+    } catch (error) {
+      console.error('Error fetching EPIC image:', error)
+    }
+  }
 
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Hello, {user ? user.name : '🪐 Galactic Voyager! 🚀'}</h1>
+
+      {epicData && (
+        <div className={styles.epicContainer}>
+          <img src={epicData.url} alt={epicData.caption} className={styles.epicImage} />
+          {epicData.caption && (
+            <h2 className={styles.epicCaption}>{epicData.caption}</h2>
+          )}
+        </div>
+      )}
     </main>
   )
 }
