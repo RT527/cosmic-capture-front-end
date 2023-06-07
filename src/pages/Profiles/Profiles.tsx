cc4b91e06b0d899cad6e35d26f242b4e24ece832
RT527 <rafi.talukder7@gmail.com>
2023-06-07 12:28:18 -0400

    profile update page

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -1,7 +1,7 @@
-import styles from './Profiles.module.css'
-import ProfileCard from '../../components/ProfileCard/ProfileCard'
-import { Profile } from '../../types/models'
-import { OpinionManagerFormData } from '../../types/forms'
+import styles from './Profiles.module.css';
+import ProfileCard from '../../components/ProfileCard/ProfileCard';
+import { Profile } from '../../types/models';
+import { OpinionManagerFormData } from '../../types/forms';
 
 interface ProfilesProps {
   profiles: Profile[];
@@ -9,21 +9,22 @@ interface ProfilesProps {
 }
 
 const Profiles = (props: ProfilesProps): JSX.Element => {
-  const { profiles } = props
+  const { profiles } = props;
 
-  if (!profiles.length) return <h1>No profiles yet</h1>
+  if (!profiles.length) return <h1>No profiles yet sorry</h1>;
 
   return (
     <main className={styles.container}>
       {profiles.map((profile: Profile) => (
-        <ProfileCard
-          key={profile.id.toString()}
-          profile={profile}
-          handleOpinion={props.handleOpinion}
-        />
+        <div className={styles.cardWrapper} key={profile.id.toString()}>
+          <ProfileCard
+            profile={profile}
+            handleOpinion={props.handleOpinion}
+          />
+        </div>
       ))}
     </main>
-  )
-}
+  );
+};
 
-export default Profiles
+export default Profiles;
